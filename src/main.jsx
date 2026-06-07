14bd51eeabaf64d71dcad412841162d2be85c18b
Mohammed Aahil <mdaahil86@gmail.com>
2026-06-07 20:29:10 +0530

    Prepare ecommerce app deployment

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ecommerce-app">
       <QueryClientProvider client={queryClient}>
         <CartProvider>
           <App />
